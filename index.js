4d3db7b7e320f970b7b58cf09bbe241de82a6362
BuildTools <unconfigured@null.spigotmc.org>
2020-12-16 11:54:17 +1100

    Added user selection and name hiding

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var msgs;
+var users;
 var jsonFetched = false;
 var ytKey;
 var jsonPath;
@@ -8,6 +9,89 @@ var muted = false;
 var isSettingUp = false;
 var justImg = false;
 var justText = false;
+var hideName = false;
+var shownMsgs = []
+var currUserID = ''
+
+function selectUser(id) {
+    console.log('Selecting: ' + id)
+
+    var num
+    for (i = 0; i < users.length; i++) {
+        if (users[i].id == id) {
+            num = i
+        }
+    }
+
+    $("#user" + num).toggleClass("selectedpfp")
+    //document.getElementById("a" + id).classList.add("selectedpfp")
+    var name = ''
+    for (i = 0; i < users.length; i++) {
+        if (users[i].id == id) {
+            name = users[i].name
+        }
+    }
+
+    $("#currusertext").text("Only showing messages from " + name)
+    currUserID = id
+}
+
+function deselectUser(id) {
+    console.log('Deselecting: ' + id)
+    var num
+    for (i = 0; i < users.length; i++) {
+        if (users[i].id == id) {
+            num = i
+        }
+    }
+    $("#user" + num).toggleClass("selectedpfp")
+    //document.getElementById("a" + id).classList.remove("selectedpfp")
+    $("#currusertext").text("Currently showing all messages")
+    currUserID = ''
+}
+
+function showOnly(id) {
+    if (id == false || id == currUserID) {
+        console.log("Already selected!")
+        deselectUser(id)
+        shownMsgs = msgs.messages
+    } else {
+        if (currUserID != '') {deselectUser(currUserID)}
+        selectUser(id)
+        shownMsgs = []
+        for (i = 0; i < msgs.messages.length; i++) {
+            if (msgs.messages[i].author.id == currUserID) {
+                shownMsgs.push(msgs.messages[i])
+            }
+        }
+    }
+}
+
+function listUsers(posts) {
+    var usersl = []
+    usersl.push(posts[0].author)
+    for (i = 1; i < posts.length; i++) {
+        var found = false
+        for (u = 0; u < usersl.length; u++) {
+            if (posts[i].author.id == usersl[u].id) {
+                found = true
+                break
+            }
+        }
+        if (found == false && posts[i].author.isBot == false) {
+            console.log(posts[i].author)
+            usersl.push(posts[i].author)
+        }
+    }
+    return usersl
+}
+
+function displayUsers(locusers) {
+    $("#userbar").html("")
+    for (i = 0; i < locusers.length; i++) {
+        $("#userbar").append('<img class="selectpfp" src="' + locusers[i].avatarUrl + '" title="' + locusers[i].name + '" id="user' + i + '" onclick="showOnly(' + locusers[i].id + ')">')
+    }
+}
 
 function togSettings() {
     if (isSettingUp == false) {
@@ -43,12 +127,42 @@ function togJustText() {
     }
 }
 
+function togHideName() {
+    if (hideName== false) {
+        hideName = true;
+        $("#hidenamecheck").attr("src", "public/check.png");
+
+        $("#profpic").toggleClass("opacnone");
+        $("#namebox").toggleClass("opacnone");
+
+        $('#namepiccombo').toggleClass('namepiccomboclass')
+
+    } else {
+        hideName = false;
+        $("#hidenamecheck").attr("src", "public/uncheck.png");
+
+        $("#profpic").toggleClass("opacnone");
+        $("#namebox").toggleClass("opacnone");
+
+        $('#namepiccombo').toggleClass('namepiccomboclass')
+    }
+}
+
 function getFile() {
     var fileUpload = document.getElementById("jsonin").files[0];
     var realfile = new FileReader;
     realfile.onload = () => {
+        currUserID = ''
+        $("#currusertext").text("Currently showing all messages")
         msgs = JSON.parse(realfile.result);
         jsonFetched = true;
+        shownMsgs = msgs.messages
+        users = listUsers(msgs.messages)
+        console.log(users)
+        displayUsers(users)
+        $('#curruserbar').removeClass('hidden')
+        $('#userbar').removeClass('hidden')
+        swal("Success!", "Loaded " + msgs.messages.length + " messages!", "success")
     }
     realfile.readAsText(fileUpload);
 }
@@ -116,11 +230,26 @@ function fadeInGuide() {
 async function loadIn() {
     var timed = setTimeout(fadeInGuide, 500);
     var envars = await remoteRequest('vars.json');
-    console.log(envars);
     envars = JSON.parse(envars);
     ytKey = envars.ytAPIKey;
-    console.log(envars);
-    console.log(msgs);
+   
+    (function() {
+        function scrollHorizontally(e) {
+            e = window.event || e;
+            var delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
+            document.getElementById('userbar').scrollLeft -= (delta * 45); // Multiplied by 40
+            e.preventDefault();
+        }
+        if (document.getElementById('userbar').addEventListener) {
+            // IE9, Chrome, Safari, Opera
+            document.getElementById('userbar').addEventListener('mousewheel', scrollHorizontally, false);
+            // Firefox
+            document.getElementById('userbar').addEventListener('DOMMouseScroll', scrollHorizontally, false);
+        } else {
+            // IE 6/7/8
+            document.getElementById('userbar').attachEvent('onmousewheel', scrollHorizontally);
+        }
+    })();
 }
 
 function newTab(url) {
@@ -153,12 +282,12 @@ async function newMsg() {
         }
         divPulse("#msgbody");
         $("#msgimg").attr("src", "");
-        var len = msgs.messages.length;
+        var len = shownMsgs.length;
         if (justImg == true) {
             do {
                 var num = Math.floor((Math.random() * len) + 1);
                 try {
-                    var imgurl = msgs.messages[num].attachments[0].url;
+                    var imgurl = shownMsgs[num].attachments[0].url;
                 } catch {
                     var imgurl = "";
                 }
@@ -167,7 +296,7 @@ async function newMsg() {
             do {
                 var num = Math.floor((Math.random() * len) + 1);
                 try {
-                    var imgurl = msgs.messages[num].attachments[0].url;
+                    var imgurl = shownMsgs[num].attachments[0].url;
                 } catch {
                     var imgurl = "";
                 }
@@ -175,21 +304,21 @@ async function newMsg() {
         } else {
             var num = Math.floor((Math.random() * len) + 1);
             try {
-                var imgurl = msgs.messages[num].attachments[0].url;
+                var imgurl = shownMsgs[num].attachments[0].url;
             } catch {
                 var imgurl = "";
             }
         }
-        var timestamp = msgs.messages[num].timestamp;
+        var timestamp = shownMsgs[num].timestamp;
         var timedate = new Date(timestamp);
         var time = formatAMPM(timedate);
         var date = timedate.getDate() + "/" + timedate.getMonth() + "/" + timedate.getFullYear();
         var fullString = date + " at " + time;
         $("#timetext").html(fullString);
-        var msg = msgs.messages[num].content;
-        var picurl = msgs.messages[num].author.avatarUrl;
+        var msg = shownMsgs[num].content;
+        var picurl = shownMsgs[num].author.avatarUrl;
         $("#profpic").attr("src", picurl);
-        var name = msgs.messages[num].author.name;
+        var name = shownMsgs[num].author.name;
         $("#nametext").html(name);
         $("#msgtext").html(msg);
         $("#msgtext").css("color", "whitesmoke");
